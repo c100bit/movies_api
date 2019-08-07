6e61eb55106d4c38ebb58c395317c427eb0086a9
c100bit <c100bit@gmail.com>
2019-08-07 22:24:39 +0300

    errors proc in render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::API
   def render(options={})
     includes = params[:include].split(',') if params[:include]
     method_name = options[:json].respond_to?(:model) ? :model : :class
-    serializer = "#{options[:json].send(method_name)}Serializer".constantize
+    class_name = options[:json].send(method_name)
     begin
+      serializer = "#{class_name}Serializer".constantize
       options[:json] = serializer.new(options[:json], include: includes)
     rescue ArgumentError
       options[:json] = serializer.new(options[:json])
     rescue
-      options[:json] = nil
     end
     super(options)
   end
